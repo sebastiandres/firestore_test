a2f8725478c49eff706b7f487db9d015fd473571
sebastiandres <sebastiandres@gmail.com>
2022-03-04 17:09:14 -0300

    Testing streamlit and firebase

---
+++ streamlit_app.py
@@ -0,0 +1,37 @@
+import streamlit as st
+from google.cloud import firestore
+
+
+try:
+    # Authenticate to Firestore with the JSON account key.
+    db = firestore.Client.from_service_account_json("firestore-key.json")
+except:
+    import json
+    key_dict = json.loads(st.secrets["textkey"])
+    creds = service_account.Credentials.from_service_account_info(key_dict)
+    db = firestore.Client(credentials=creds, project="streamlit-reddit")    
+
+
+# Streamlit widgets to let a user create a new post
+c1, c2, c3 = st.columns([2,2,1])
+title = c1.text_input("Post title")
+url = c2.text_input("Post url")
+submit = c3.button("Submit")
+
+# Once the user has submitted, upload it to the database
+if title and url and submit:
+	doc_ref = db.collection("posts").document(title)
+	doc_ref.set({
+		"title": title,
+		"url": url
+	})
+
+# And then render each post, using some light Markdown
+posts_ref = db.collection("posts")
+for doc in posts_ref.stream():
+	post = doc.to_dict()
+	title = post["title"]
+	url = post["url"]
+
+	st.subheader(f"Post: {title}")
+	st.write(f":link: [{url}]({url})")
