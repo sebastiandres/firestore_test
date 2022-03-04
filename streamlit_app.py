ad883e2f71f900dc176f5da67513ef43c1c9f1a6
sebastiandres <sebastiandres@gmail.com>
2022-03-04 17:22:43 -0300

    fixing next error

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ except:
     import json
     key_dict = json.loads(st.secrets["textkey"])
     creds = service_account.Credentials.from_service_account_info(key_dict)
-    db = firestore.Client(credentials=creds, project="streamlit-reddit")    
+    db = firestore.Client(credentials=creds)#, project="streamlit-reddit")    
 
 
 # Streamlit widgets to let a user create a new post
